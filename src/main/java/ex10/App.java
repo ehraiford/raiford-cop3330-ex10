931f8b265c9b7035546e154ef710942db5706608
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-04 21:32:50 -0400

    Completed code.

--- src/main/java/ex10/App.java
+++ src/main/java/ex10/App.java
@@ -4,8 +4,30 @@
  */
 package ex10;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        DecimalFormat dec = new DecimalFormat("####.##");
 
+        double subtotal = 0, tax = .055, total;
+        int quantity;
+        for(int ticker = 0; ticker < 3; ticker++)
+        {
+            System.out.print("Enter the price of item "+ (ticker+1)+ ": ");
+            double item = in.nextDouble();
+            System.out.print("Enter the quantity of item " + (ticker+1) + ": ");
+            quantity = in.nextInt();
+            subtotal = subtotal + (item * quantity);
+        }
+
+        tax = tax * subtotal;
+        total = subtotal + tax;
+
+        System.out.println("Subtotal: $" + dec.format(subtotal));
+        System.out.println("Tax: $" + dec.format(tax));
+        System.out.println("Total: $" + dec.format(total));
     }
 }
